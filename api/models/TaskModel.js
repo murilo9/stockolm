d09a32e5cbd07b3be7548fcb4b98664aa0f01e9f
Murilo <murilo@yousendr.com>
2019-07-30 22:21:59 -0300

    Add Task.update; Correções em Task.create

--- api/models/TaskModel.js
+++ api/models/TaskModel.js
@@ -45,14 +45,14 @@ exports.create = (username, task, next) => {
         var details = task.details ? task.details : ''
         var startDateString = task.startDate ? task.startDate.getTime() : null
         var endDateString = task.endDate ? task.endDate.getTime() : null
-        var startDate = startDateString ? `${startDateString.getFullYear()}-
-        ${startDateString.getMonth()+1}-${startDateString.getDate()} 
-        ${startDateString.getHours()}-${startDateString.getMinutes()}-
-        ${startDateString.getSeconds()}` : null
-        var endDate = endDateString ? `${endDateString.getFullYear()}-
-        ${endDateString.getMonth()+1}-${endDateString.getDate()} 
-        ${endDateString.getHours()}-${endDateString.getMinutes()}-
-        ${endDateString.getSeconds()}` : null
+        var startDate = startDateString ? `${task.startDate.getFullYear()}-
+            ${task.startDate.getMonth()+1}-${task.startDate.getDate()} 
+            ${task.startDate.getHours()}-${task.startDate.getMinutes()}-
+            ${task.startDate.getSeconds()}` : null
+        var endDate = endDateString ? `${task.endDate.getFullYear()}-
+            ${task.endDate.getMonth()+1}-${task.endDate.getDate()} 
+            ${task.endDate.getHours()}-${task.endDate.getMinutes()}-
+            ${task.endDate.getSeconds()}` : null
         var priority = task.priority
         var status = task.status
         //Insert into DB:
@@ -71,4 +71,42 @@ exports.create = (username, task, next) => {
         })
     }
     
+}
+
+exports.update = (username, newTask, taskId, next) => {
+    //Gets new task variables:
+    var name = newTask.title ? newTask.title : null
+    var details = newTask.description ? newTask.description : null
+    var startDateString = newTask.startDate ? newTask.startDate.getTime() : null
+    var startDate = newTask.startDate ? `${newTask.startDate.getFullYear()}-
+        ${newTask.startDate.getMonth()+1}-${newTask.startDate.getDate()} 
+        ${newTask.startDate.getHours()}-${newTask.startDate.getMinutes()}-
+        ${newTask.startDate.getSeconds()}` : null
+    var endDateString = newTask.endDate ? newTask.endDate.getTime() : null
+    var endDate = endDateString ? `${newTask.endDate.getFullYear()}-
+        ${newTask.endDate.getMonth()+1}-${newTask.endDate.getDate()} 
+        ${newTask.endDate.getHours()}-${newTask.endDate.getMinutes()}-
+        ${newTask.endDate.getSeconds()}` : null
+    var priority = newTask.priority
+    var status = newTask.status
+    //Update into DB:
+    var script = "UPDATE tbTarefas SET "
+    if(name) script += `nome = '${name}'`
+    if(details) script = `, detalhes = '${details}'`
+    if(startDate) 
+        script += `, dataInicio = '${startDate}', dataInicioString = '${startDateString}'`
+    if(endDate)
+        script += `, dataFim = '${endDate}', dataFimString = '${endDateString}'`
+    if(priority) script += `, prioridade = ${priority}`
+    if(status) script += `, status = ${status}`
+    script += ` WHERE id = ${taskId}`
+    DB.con(username).query(script, (err, result, fields)=>{
+        if(err){
+            console.log(err)
+            next(false)
+        }
+        else{
+            next(true)
+        }
+    }
 }
